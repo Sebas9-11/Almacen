346ce0412363b62de93e0f7299b730c7b203e91f
Sebas9-11 <81379148+Sebas9-11@users.noreply.github.com>
2021-04-22 11:43:09 -0500

    Add files via upload

---
+++ JavaScript/empleossp.js
@@ -0,0 +1,100 @@
+const abrir1 = document.getElementById("abrir1");
+const guardar1 = document.getElementById("guardar1");
+const contenedor_abrir1 = document.getElementById("contenedor-abrir1");
+const contenedor_guardar1 = document.getElementById("contenedor-guardar1");
+const cerrar1 = document.getElementById("cerrar1");
+const cerrar2 = document.getElementById("cerrar2");
+
+
+abrir1.addEventListener("click", () =>{
+    contenedor_abrir1.classList.add("show");
+});
+
+guardar1.addEventListener("click", () =>{
+    contenedor_guardar1.classList.add("show");
+});
+
+cerrar1.addEventListener("click", () =>{
+    contenedor_abrir1.classList.remove("show");
+});
+
+cerrar2.addEventListener("click", () =>{
+    contenedor_guardar1.classList.remove("show");
+});
+
+
+const abrir2 = document.getElementById("abrir2");
+const guardar2 = document.getElementById("guardar2");
+const contenedor_abrir2 = document.getElementById("contenedor-abrir2");
+const contenedor_guardar2 = document.getElementById("contenedor-guardar2");
+const cerrar3 = document.getElementById("cerrar3");
+const cerrar4 = document.getElementById("cerrar4");
+
+
+abrir2.addEventListener("click", () =>{
+    contenedor_abrir2.classList.add("show");
+});
+
+guardar2.addEventListener("click", () =>{
+    contenedor_guardar2.classList.add("show");
+});
+
+cerrar3.addEventListener("click", () =>{
+    contenedor_abrir2.classList.remove("show");
+});
+
+cerrar4.addEventListener("click", () =>{
+    contenedor_guardar2.classList.remove("show");
+});
+
+
+const abrir4 = document.getElementById("abrir4");
+const guardar4 = document.getElementById("guardar4");
+const contenedor_abrir4 = document.getElementById("contenedor-abrir4");
+const contenedor_guardar4 = document.getElementById("contenedor-guardar4");
+const cerrar7 = document.getElementById("cerrar7");
+const cerrar8 = document.getElementById("cerrar8");
+
+
+abrir4.addEventListener("click", () =>{
+    contenedor_abrir4.classList.add("show");
+});
+
+guardar4.addEventListener("click", () =>{
+    contenedor_guardar4.classList.add("show");
+});
+
+cerrar7.addEventListener("click", () =>{
+    contenedor_abrir4.classList.remove("show");
+});
+
+cerrar8.addEventListener("click", () =>{
+    contenedor_guardar4.classList.remove("show");
+});
+
+
+const abrir3 = document.getElementById("abrir3");
+const guardar3 = document.getElementById("guardar3");
+const contenedor_abrir3 = document.getElementById("contenedor-abrir3");
+const contenedor_guardar3 = document.getElementById("contenedor-guardar3");
+const cerrar5 = document.getElementById("cerrar5");
+const cerrar6 = document.getElementById("cerrar6");
+
+
+abrir3.addEventListener("click", () =>{
+    contenedor_abrir3.classList.add("show");
+});
+
+guardar3.addEventListener("click", () =>{
+    contenedor_guardar3.classList.add("show");
+});
+
+cerrar5.addEventListener("click", () =>{
+    contenedor_abrir3.classList.remove("show");
+});
+
+cerrar6.addEventListener("click", () =>{
+    contenedor_guardar3.classList.remove("show");
+});
+
+
